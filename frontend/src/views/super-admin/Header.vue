f4ec1f04bd9132c0adba02f7408e7089699ff819
jandrei-carlum <119277162+jandrei-carlum@users.noreply.github.com>
2024-05-17 15:34:12 +0800

    Header
    new header

---
+++ frontend/src/views/super-admin/Header.vue
@@ -0,0 +1,59 @@
+<!-- Main navigation container -->
+
+
+
+<template>
+<nav
+  class="bg-test h-14 relative flex w-full flex-wrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:py-4">
+  <div class="flex w-full flex-wrap items-center justify-between px-3">
+    <nav class="w-full rounded-md" aria-label="breadcrumb">
+      <ol class="list-reset ms-2 flex">
+        <li>
+          <a
+            href="#"
+            class="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80"
+            >Home</a
+          >
+        </li>
+        <li>
+          <span class="mx-2 text-black/60 dark:text-white/60">/</span>
+        </li>
+        <li>
+          <a
+            href="#"
+            class="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80"
+            >Library</a
+          >
+        </li>
+        <li>
+          <span class="mx-2 text-black/60 dark:text-white/60">/</span>
+        </li>
+        <li>
+          <a
+            href="#"
+            class="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80"
+            >Data</a
+          >
+        </li>
+      </ol>
+    </nav>
+  </div>
+</nav>
+
+</template>
+
+<style>
+.bg-test{
+    background-color: aqua;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
