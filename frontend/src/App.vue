20610c5be716f73a3e246af59be41b10a11b24ba
Harold Angelo Talagtag <hatalagtag@gmail.com>
2024-06-03 18:05:51 +0800

    new-update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,67 +1,27 @@
-<!-- <script setup>
-import User from './components/User.vue' //Para mapakita sa screen | Feel ko eto ung landing page | File name yung "User"
-</script> -->
-
-<!-- <template> 
-
-  <User/> 
-</template> -->
-
 <template>
-	<div class="app">
-		<!-- Sidebar -->
-		<Sidebar />
-
-		<!-- Content -->
-		<router-view />
+	<div id="app" :class="{ 'dark': isDarkMode }">
+	  <!-- Your page content -->
+	  <router-view />
 	</div>
-</template>
-
-<script setup>
-import Sidebar from './components/Sidebar.vue'
-
-</script>
-
-<style lang="scss">
-:root {
-	--primary: #4ade80;
-	--primary-alt: #22c55e;
-	--grey: #64748b;
-	--dark: #1e293b;
-	--dark-alt: #334155;
-	--light: #f1f5f9;
-	--sidebar-width: 300px;
-}
-
-* {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-	font-family: 'Fira sans', sans-serif;
-}
-
-body {
-	background: var(--light);
-}
-
-button {
-	cursor: pointer;
-	appearance: none;
-	border: none;
-	outline: none;
-	background: none;
-}
-
-.app {
-	display: flex;
-
-	main {
-		flex: 1 1 0;
-		padding: 2rem;
-
-		@media (max-width: 1024px) {
-			padding-left: 6rem;
-		}
+  </template>
+  
+  <script>
+  export default {
+	data() {
+	  return {
+		isDarkMode: false
+	  }
+	},
+	methods: {
+	  toggleMode() {
+		this.isDarkMode = !this.isDarkMode;
+	  }
 	}
-}
-</style>
+  }
+  </script>
+  
+  <style>
+  .dark {
+	/* Styles for dark mode */
+  }
+  </style>
---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <Sidebar v-if="$route.meta.sidebar"></Sidebar>
+      <router-view></router-view>
+    </div>
+  </template>
+  
+  <script>
+  import Sidebar from '../components/Sidebar.vue';
+  
+  export default {
+    components: {
+      Sidebar,
+    },
+  };
+  </script>
